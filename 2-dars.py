4e65fdfb01370fe2ef44b3f8f5c37934ac3c8ee9
javohir <joj102522@gmail.com>
2023-03-10 16:03:01 +0500

    Initial commit

---
+++ 2-dars.py
@@ -0,0 +1,59 @@
+son10 = 99
+son20 = 66
+natija = son10 * son20
+print(natija)
+
+
+a=90
+b=4
+j= a * b
+print(j/4)
+
+s=19479
+b=3729
+j=s - b
+print(j+27498658654)
+
+a=7
+b=3
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+a=38
+s=36
+print(a > s)
+print(a < s)
+
+a='salom'
+b=True
+d=5
+e=5.3
+f=3.7
+print(d+e)
+
+a=8
+b=8
+print(a==b)
+print(a!=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3-dars.py
@@ -0,0 +1,29 @@
+import math
+
+a = math.pi
+print(a)
+
+a = 7
+b = 7
+c = a*b
+print(c)
+
+print(math.sqrt(c)) #ildizdan chiqarish
+
+print(math.ceil(45.6)) #katta yaqiniga
+
+print(math.floor(45.67)) #kichik yaqinligi
+
+print(math.sin(90)) #sonning sin si
+
+print(math.factorial(37)) #1*2*3*4*5
+
+print(math.log(25, 5)) #(5*5*5)==5**3
+
+print(math.cos(10))
+
+
+
+
+
+
