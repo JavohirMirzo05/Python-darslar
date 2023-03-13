cc8a4d4237d95fc399e7e46f29701173aab99027
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-13 14:27:41 +0500

    4-dars
    4-dars

---
+++ 4-dars.py
@@ -0,0 +1,75 @@
+# isimlar = ['Dilshod', 'Anvr', 'Sardor', 'Dilmurod', 'Bobojon']
+
+# print('Salom ' + isimlar[0] + ' ishlaring yahshimi?')
+# print('Salom ' + isimlar[1] + ' uy ishi qildingmi?')
+# print('Salom ' + isimlar[2] + ' sagliging qanday?')
+
+# print(f'Salom {isimlar[0]}, {isimlar[1]}, {isimlar[2]} ishlaring yahshimi?')
+
+# yil=int(input("Tug'ilgan yilingiz: "))
+# print(f"Siz {2022-yil} yoshdasiz." )
+
+# .............................................................................
+
+# sonlar = [11, 27, 648, 749, 7584, 657_688_678, 1.]
+
+# sonlar[0] = sonlar[0] + sonlar[-1]
+# sonlar[1] = -99.7
+# sonlar[4] = sonlar[4] + 84
+# del sonlar[5]
+# del sonlar[0]
+# print(sonlar)
+# print(sonlar[2])
+
+
+
+
+# t_shaxslar = ["Amit Temur", "Imom Buhoriy", "Napalion"]
+
+# z_shaxslar = ["Bill Gets", "Elon Musk", "Stif Jobs"]
+
+# print(f"\nMen tarixiy shaxslardan {t_shaxslar.pop(0).title()} bilan,\n\
+# zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan\n\
+# suhbat qurishni istar edim\n")
+
+# print(f"""Men tarixiy shaxslardan {t_shaxslar.pop(-1).title()} bilan
+# zamonaviy shaxslardan esa {z_shaxslar.pop(-1).title()} bilan
+# suhbat qurishni istar edim""")
+
+
+# friends = []
+# friends.append("Anvar")
+# friends.append("Vali")
+# friends.append("Davlat")
+# friends.append("Sarvar")
+# friends.append("Eldor")
+# print(f"To'liq ro'yhat-{friends}")
+
+
+# friends.remove("Anvar")
+# friends.remove("Vali")
+# friends.remove("Davlat")
+# print(f"Kelganlar-{friends}")
+
+
+# friends.append("Hasan")
+# friends.insert(0, "Husan")
+# friends.insert(2, "Ivan")
+# print(friends)
+
+# mehmonlar = ["Alisher","Anvar","Jamshid","Dilshod"]
+# mehmonlar.append(friends.pop(1))
+# mehmonlar.append(friends.pop(-1))
+# mehmonlar.append(friends.pop(0))
+# print("\nKelgan mehmonlar: ", mehmonlar)
+
+
+
+
+
+
+
+
+
+
+
