053bbeda81e75cb780aa5c3e6add43317b1d5271
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-14 19:17:16 +0500

    5-dars
    5-dars

---
+++ 5-dars.py
@@ -0,0 +1,85 @@
+ # TARTIBLASH
+# cars = ["ru", "yeih", "bhrguyc", "ueruytrg","ahhuh"]
+# print(cars)
+# cars.sort()
+# print(cars)
+
+# ...............................................................................
+# Katt va kichik harflar
+# cars = ["ru", "yeih", "bhrguyc", "Tjhsdgcg","ueruytrg","ahhuh", "Snkjfhcjbb"]
+# print(cars)
+# cars.sort()
+# print(cars)
+
+# .......................................................................
+# Teskari tartib
+# cars = ["ru", "yeih", "bhrguyc", "Tjhsdgcg","ueruytrg","ahhuh", "Snkjfhcjbb"]
+# print(cars)
+# cars.sort(reverse=True) #z-a 
+# print(cars)
+# print(sorted(cars)) #a-z
+# print(cars)
+
+ #........................................................
+# mehmonlar=["Anton", "Vali", "Hasan", "Po'lat"]
+# print("Sorted() a-z dan qaytargan ro'yhat:", sorted(mehmonlar))
+# print("asl ro'uhat o'zgarmas qoladi:", mehmonlar)
+# print(sorted(mehmonlar, reverse=True))
+# print("Asl ro'yhat o'zgarmas qoladi:", mehmonlar)
+
+#..............................................
+# a=[29,859,80,4,6868,1,797,7]
+# # a.sort() #a-z
+# print(sorted(a))
+# print(sorted(a, reverse=True))
+# print(a)
+
+#............................................................
+
+# a1=input("ismingizni kriting: ")
+# a2=input("ismingizni kriting: ")
+# a3=input("ismingizni kriting: ")
+# a4=input("ismingizni kriting: ")
+# a5=input("ismingizni kriting: ")
+# a6=input("ismingizni kriting: ")
+# friends=[]
+# friends.append(a1)
+# friends.append(a2)
+# friends.append(a3)
+# friends.append(a4)
+# friends.append(a5)
+# friends.append(a6)
+# friends.sort()
+# print(sorted(friends))
+# print(friends)
+ #......................................................................
+#Ro'yhatni ortidan boshlab chiqaish
+# asr = ["dy", "dh", "dy", "eg", "eyg"]
+# asr.reverse()
+# print(asr)
+
+# son = [4, 5, 7, 9]
+# son.reverse()
+# print(son)
+
+
+#ro'yhatni uzunligi
+# o = ["sd", "ty", "jo", "gju"]
+# print("element soni:",len(o) )
+
+
+
+
+# narhlar = [278,7484789,8798547,798,764657,65876575]
+# arzon = min(narhlar)
+# qimmat= max(narhlar)
+# jami= sum(narhlar)
+# print("eng arzon narx", arzon, ". eng qimmati", qimmat, ".   jami", jami)
+# print(f"eng arzon narh: {arzon}. eng qimmat: {qimmat}. jami: {jami}")
+
+
+
+
+
+
+
