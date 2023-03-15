af8f058d7afa6349c043fa43feb3acadbeeb1308
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-15 13:58:46 +0500

    6-dars.py
    6-dars.py

---
+++ 6-dars.py
@@ -0,0 +1,91 @@
+#   # RANGE()
+sonlar=list(range(1, 10, 3))
+print(sonlar)
+
+
+# RANGE va qadam  15,  35
+# toq_sonlar=list(range(15,35,2))
+# print("toq_son:", toq_sonlar)
+
+
+#RO"YHATNI KESISH
+# cars=["olma", "anor", "o'rik", "banan", "olho'ri"]
+# my_cars = cars[1:3]
+# print(cars)
+# print(my_cars)
+# print(cars[:4])
+# print(cars[2])
+# print(cars[1:3])
+
+#RO"YHATDAN NUSHA OLISH
+# sonlar= [1,2,3,4,5,6,]
+
+# sonlar2 = sonlar[:]
+# sonlar2.append(7)
+# sonlar2.append(8)
+# print("bu sonlar ro'yhati:", sonlar)
+# print("bu sonlar 2 to'yhati:", sonlar2)
+
+#......................................................
+
+# sonlar=list(range(22,68))
+# print(sonlar)
+# sonlar2 =sonlar[:]
+# sonlar2.append(88)
+# sonlar2.append(12)
+# sonlar2.append(0)
+# sonlar2.append(99)
+# print("bu sonlar ro'yhati:", sonlar)
+# print("bu sonlar 2 to'yhati:", sorted(sonlar2))
+
+
+# # TUPLES
+# tomonlar=(20,30,55.4)
+# # tomonlar.pop(0)
+# print(tomonlar)
+
+# toys=("bus","car", "bear", "dino")
+# print(toys[0])
+# print(toys[-1])
+# print(toys[:5])
+
+
+#TUPLES<->LIST
+# toys=("bus","car", "bear", "dino")
+# toys=list(toys)
+
+# toys.append("daragon")
+# toys.remove("bus")
+# toys[1]="masqueen"
+# toys=tuple(toys)
+# print(toys)
+
+
+
+
+# raqam=(97_173_55_33, 81_903_81_01, 33_330_95_01)
+# raqam=list(raqam)
+
+# raqam.append(33_613_30_40)
+# raqam.remove(97_173_55_33)
+# raqam[1]
+# raqam=tuple(raqam)
+ # print(raqam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
