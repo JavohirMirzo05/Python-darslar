0131cd65ba84a54ebc7d14f179505c8c280955ea
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-16 14:54:13 +0500

    7-dars
    7-dars

---
+++ 7-dars.py
@@ -0,0 +1,49 @@
+# for operatori
+# mehmonlar = ["Ali", "Vali", "Hasan", "Husan", "Olim"]
+# for mehmon in mehmonlar:
+#     print(mehmon)
+#     print("Dasturtugadi")
+
+# sonlar = [1,2,3,4,5]
+# for a in sonlar:
+#     # print(a)
+#     print(a, end=", ")
+
+
+# a = 1
+# mehmonlar = ["Ali", "Vali", "Hasan", "Husan", "Olim"]
+# for mehmon in mehmonlar:
+#     print(f"{a}. Hurmatli {mehmon},sizni 20 dekabr kuni o'tadigan el oshiga taklif qilamz")
+# print("Hurmat bilan Eshonqulovlar oilasi")
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son**2}ga teng")
+#     print(f"{son} ning kubi {son**3}ga teng, \n")
+
+# sonlar = list(range(1,6))
+# for son in sonlar:
+#         print(f"{son} ning darajasi {son**son}ga teng")
+        
+        
+# sonlar = list(range(11))
+# sonlar_kvadrati =  []
+# for son in sonlar:
+#     sonlar_kvadrati.append(son**2)
+# print(f"sonlar:      {sonlar}")
+# print(f"sonlar_kvadrati: {sonlar_kvadrati}")
+        
+# dostlar = [ ]
+# print("5 ta eng yaqin do'stingiz kim")
+# for n in range(1,  6):
+#     dostlar.append(int(input(f"{n}-do'stingizni kiriting:")))
+# print(dostlar)
+
+        
+# a_1 = range (1,    10)
+# b_1 = range (1,   11)
+# for a in a_1:
+#     for b in b_1:
+#         print(f"formulasi: a*b; {a} * {b} = {a*b}")
+        
+        
