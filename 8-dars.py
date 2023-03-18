72d7794bbd0477f435ebbfb6fd908a70eced670a
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-18 19:20:35 +0500

    8-dars
    8-dars

---
+++ 8-dars.py
@@ -0,0 +1,70 @@
+# avtolar=["audi", "bmw", "volvo", "kia", "hyundai"]
+# for k in avtolar:#avtolar ichida har bir avto uchun...
+#     if k == "bmw":#...agar avto bmw ga tend bo'lsa...
+#         print(k.upper())#avto nomini hamma hariflarni ham katta bilan
+#     else:# aks holda
+#         print(k.title())#avto nomini faqat 1harifini katta qiladi
+
+
+#.............................................................................
+
+# ism = input("Ismingizni kriting\n>>> ")
+# if ism.lower() != "javohir":
+#     print(f"Uzr, {ism.title()} biz Javohirni kutayapmiz. ")
+# else:
+#     print("Salom, Javohir")
+
+#.............................................................................
+
+# javob = float(input("80x10 nechiga teng?\n>>> "))
+# if javob != 800:
+#   print("Javob xato!")
+# else:
+#   print("Javob to'g'ri!")
+
+
+#............................................................................
+
+# yosh = int(input("Yoshingiz nechida?\n>>> "))
+# if yosh >=18:
+#  print("Xush kelibsiz!")
+# else:
+#  print("Krish mumkun emas!")
+
+#...............................................................................
+
+# login = input("Yangi login kriting!\n>>> ")
+# if len(login)<=6:
+#   print("Login 6ta harfdan ko'p bo'lishi kerak!")
+# else:
+#   print("Login qabul qilindi!")
+ 
+ #.............................................................................
+ 
+# yil = int(input("Tug'ilgan yilingizni kriting: "))
+# if 2022-yil<17: 
+#  print(f"Yoshingiz{20022-yil}da ekan.")
+#  print("Krish mumkun emas!")
+# else: 
+#  print("Xush kelibsiz!")
+ 
+ #................................................................
+ 
+# cars = ["ls", "jug", "huy", "jbu"]
+# for car in cars: 
+#   if car=="huy":
+#     print(car.upper())
+#   else:
+#     print(car.title())
+ 
+#...............................................................
+    
+# yosh=int(input("Yoshingiz nechida?\>>> ")) 
+# if yosh > 65:    
+#   print("Siz kovid guruhida emassiz!")
+# else:
+#   print("siz kovid guruhidasiz!")   
+ 
+ 
+ 
+ 
