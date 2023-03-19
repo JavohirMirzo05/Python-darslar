4cc8eae6cf41348fe2d39f10fc7aec3adf0acd37
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-19 21:48:55 +0500

    9-dars
    9-dars

---
+++ 9-dars.py
@@ -0,0 +1,31 @@
+# isim=input("Talaba isim familyasini kriting:\n>>> ")
+# baho= int(input(f"{isim. title()}ni baholang\n>>> "))
+# if baho == 5:
+#   print("Alo")
+# if baho ==4:
+#   print("Yahshi")
+# if baho <=3:
+#   print("Yomon")
+
+#....................................................................
+
+# menu=["osh, somsa, shashlik, norin "]
+# ovqat= input("Nima ovqat yeysiz?\n>>> ")
+# if ovqat.lower() not in menu:
+#  print("buyurtma qabul qilindi")
+# else:
+#    print("Avsuski bizda bunday ovqat yo'q")
+
+#...................................................................
+# menu=[]
+# for a in range(1, 6):
+#     menu.append(input(f"{a}-ta'omni kriting:\n>>> ")) 
+# taom=input("Nima taom buyurtma qilasiz?: ")
+# if taom not in menu:
+#     print(f"Kechirasiz bizda {taom} yo'q! Bizdagi bizdagi taomlar ro'yhati: ")
+# for a in menu:   
+#     print(a, end=(f"\n"))
+# taom_2=input("Iltimos quyidagi taomlardan buyurtma qiling: ")
+#     print(f"{taom_2} qabul qilindi! ")
+# else:
+#     print(f"{'taom'} qabul qilindi! ")
