4bea424b484ad8f07b3d83f6dc625d1d807f3cf8
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-22 18:43:38 +0500

    11-dars
    11-dars

---
+++ 11-dars.py
@@ -0,0 +1,39 @@
+import math as m
+
+a=int(input("a ni kriting!\n>>> "))
+b=int(input("b ni kriting!\n>>> "))
+d=int(print("d ni kriting!\n>>> "))
+
+c = m.sqrt(a*b/m.sqrt(d+2))
+print(c)
+
+# ......................................................................
+
+
+# car_0={"model": "ferariy", "rang": "qizil"}
+# print(car_0["model"])
+# print(car_0["rang"])
+
+# # Lug'at istalgan malumotni saqlashi mumkun
+# talaba={
+#         "ism": "murod olimov",
+#         "yosh": "22",
+
+#         "t_yil": "2006"
+#         }
+# print(f"{talaba['ism'].title()}\
+#       {talaba['t_yil']}-yilda tug'lgan,\
+#       {talaba['yosh']}yoshda")
+      
+#.......................................................................
+# talaba['kurs'] = 4     
+# talaba['fokulted'] = 'informatika'      
+# talaba['ism'] = 'abdullaoh'     
+      
+# print(f"{talaba['ism'].title()},\
+#       {talaba['t_yil']}-yilda tug'lgan,\
+#       {talaba['yosh']}yoshda,\
+#       {talaba['fokulted']}fokulted,\ 
+#       # {talaba{['kurs']} kursda")
+
+
