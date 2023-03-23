8748f0c86a2fd29bcd34a14bded9b2d8341a73e7
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-23 19:20:28 +0500

    12-dars
    12-dars

---
+++ 12-dars.py
@@ -0,0 +1,145 @@
+
+# talaba={}
+# talaba["ism"]='qobil rasulov'
+# talaba["kurs"] = 3
+# talaba["yosh"] = 20
+# print(talaba)
+# print(f"Talaba {talaba['ism'].title()} {talaba['kurs']}-kurs")
+
+#...........................................................................
+
+# talaba["kurs"] = 4
+# print(f"Talaba {talaba['ism'].title()} {talaba['kurs']}-kurs")
+
+#...........................................................................
+
+# telefonlar={
+#     "ali":"ipot x",
+#     "vali":"ipot 14",
+#     "olim":"mi 10 pro",
+#     "orif":"no. 1"
+#     }
+
+
+# phone=telefonlar["ali"]
+# print(f"Alini telefoni {phone}")
+
+
+# phone=telefonlar.get("olim", "Bunday ism mavjud emas")
+# print(phone)
+
+#........................................................................
+
+# sonlar = {}
+# a = int(input("a >>> "))
+# b = int(input("b >>> "))
+# sonlar['a']=a
+# sonlar['b']=b
+# print(sonlar)
+
+# print(f"a={sonlar['a']}")
+
+#............................................................................
+
+# talaba={
+#         'ism':'ali',
+#         'familya':'valiev',
+#         'yosh':20,
+#         'fakulted':'fizika',
+#         'kurs':4
+#         }
+# key=input("kalit sozini kriting:\n>>>  ")
+# if key.lower() in talaba:
+#     print(talaba[key])
+# else:
+#     key=input("bunday kalit soz mavjud emas iltimos kalit soz kriting.\nkey>>> ")
+#     value=input("Value>>> ")
+#     talaba[key]=value
+#     print(f"{key}--{talaba[key]}")
+    
+   #................................................................
+   
+# for kalit, qiymat in talaba.items():
+#     print(f"Kalit: {kalit}")
+#     print(f"Qiymat: {qiymat}\n>>> ")
+   
+# ............................  
+# telefonlar={
+#         "ali":"ipot x",
+#         "vali":"ipot 14",
+#         "olim":"mi 10 pro",
+#         "orif":"no. 1"
+#         }
+# for key, value in  telefonlar.items():
+#     print(f"{key.title()}ning telefoni {value}")
+   
+#.......................................................................  
+   
+# mahsulotlar={
+#     'olma':100,
+#     'anor':200,
+#     'nok':300,
+#     'uzum':400,
+#     'anjir':500
+#     }  
+# print("da'kondagi mahsulotlar:") 
+# for mahsulot in mahsulotlar.key():
+#     print(mahsulotlar.title())
+    
+# bozorliklar=['anor', 'uzum', 'nok', 'baliq']   
+# for mahsulot in  mahsulotlar:   
+#     if mahsulot in bozorliklar:
+#         print(f"{mahsulot.title()} {mahsulotlar[mahsulot]}som")
+    
+# for buyum in bozorliklar:
+#     if buyum not in mahsulotlar:
+#         print(f"iltimos, dokoningizga {buyum} ham olib keling ")
+   
+#......................................................................
+#   Uy ishi
+   
+# otam={}
+# otam["ism"]='E. jahongir'
+# otam["t_yil"] = 1981
+# otam["yosh"] = 41 
+# print(otam)
+# print(f"Otam {otam['ism'].title()} {otam['t_yil']}-yil. Hozirda {otam['yosh']} yoshda. ")      
+       
+   
+   
+# onam={}
+# onam["ism"]='E. guljamol'
+# onam["t_yil"] = 1983
+# onam["yosh"] = 39 
+# print(onam)
+# print(f"Otam {onam['ism'].title()} {onam['t_yil']}-yil. Hozirda {onam['yosh']} yoshda. ")        
+    
+    
+# ukam={}
+# ukam["ism"]='A. muhammat'
+# ukam["t_yil"] = 2015
+# ukam["yosh"] = 7 
+# print(ukam)
+# print(f"Otam {ukam['ism'].title()} {ukam['t_yil']}-yil. Hozirda {ukam['yosh']} yoshda. ")            
+    
+    
+# taomlar={}
+# taomlar["osh"]='otmning sevimli taomi osh.'
+# taomlar["manti"] ='ukamning sevimli taomi manti.'
+# taomlar["somsa"] = 'oyimning sevimli taomi somsa.'
+# print(taomlar)
+# print(f"Taomlar {taomlar['osh'].title()}\n {taomlar['manti'].title()}\n {taomlar['somsa'].title()}")    
+
+
+# belgilar={
+#     'input':'krituvchi',
+#     'print':'chiqaruvchi',
+#     'import math':'misol-masalalar ishlayotganda ishlatiladi',
+#     'append':'krituvchi',
+#     'remove':'chiqaruvchi',
+#     'sort()':'tartiblaydi'}
+
+
+
+
+
