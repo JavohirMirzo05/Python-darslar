d2aae309b6753fed8e4b7e09bab52e7b1da5eb86
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-27 16:35:35 +0500

    14-2 dars.py
    14-2 dars.py

---
+++ 14-2 dars.py
@@ -0,0 +1,78 @@
+
+# son =0
+# while son<10:
+#     son +=1
+#     if son%2==0:
+#         continue
+#     else:
+#         print(son)
+
+
+
+# import math
+# print("Kritilgan sonni ildizini qaytaruvchi dastur.")
+# savol="Istalgan son kritining "
+# savol += "(dasturni to'htatish uchun 'exit' deb yozing)\n>>> "
+# while True:
+#     son= input(savol)
+#     if son == 'exit':
+#         break
+#     else:
+     
+#         print(math.sqrt(float(son)))
+       
+# print("Dastur to'xttadi!")
+
+
+
+
+# import math
+# print("Kritilgan sonni ildizini qaytaruvchi dastur.")
+# savol="Istalgan son kritining "
+# savol += "(dasturni to'htatish uchun 'exit' deb yozing)\n>>> "
+# while True:
+#     son= input(savol)
+#     if son == 'exit':
+#         break
+#     else:
+#          if int(son) < 0:
+#             print("iltimos musbat son kriting ") 
+#          else:
+#              print(math.sqrt(float(son)))
+# print("Dastur to'xttadi!")
+
+
+# son =0
+# while son<10:
+#     son +=1
+#     if son%2!=0:
+#         continue
+#     else:
+#         print(son)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
