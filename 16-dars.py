bb4ecef094b3fde8fc20edf8a49be716028f1158
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-03-28 14:48:58 +0500

    16-dars.py
    16-dars.py

---
+++ 16-dars.py
@@ -0,0 +1,97 @@
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalomu alaykum")
+
+
+
+
+# def salom_ber(ism, familya):
+#     """Foydalanuvchi isim qabul qilib,
+#     unga salom beruvchi funksiya"""
+#     print(f"Assalomu alayfum, hutmatli {ism.title()} {familya.title()}")
+
+# salom_ber("hasan","olimov")
+# salom_ber("olim", "olimov")
+
+
+# def toliq_ism(ism, familya):
+#     """foydalanuvchi ism va familyasini chiqaruchi funksiya"""
+#     print(
+#         f"Foydalanuvchi ismi: {ism.title()}\n "
+#         f"Foydalanuvchi familyasi: {familya.title()}\n "
+#         )
+
+# toliq_ism("olim", "hokimov")
+# toliq_ism("olim", "hokimov")
+
+
+# def yosh_hisobla(ism, tugulgan_yil ):
+#     """Foydalanuvchi yoshini hisoblaydigan dastur"""
+#     print(f"{ism.title()} {2022-tugulgan_yil} yoshda")
+
+
+# yosh_hisobla("olim,", 2006)
+
+
+# def yosh_hisobla(tugulgan_yil, joriy_yil=2022):
+#     """Foydalanuvchi tugulgan yilidan uning yoshini aniqlovchi dastur"""
+    # print(f"Siz {joriy_yil-tugulgan_yil} yoshdasiz")
+
+# yosh_hisobla(2006, joriy_yil=2022)
+
+
+# def yosh_hisobla(tugulgan_yil, joriy_yil=2022):
+#     """Foydalanuvchi tugulgan yilidan uning yoshini aniqlovchi dastur"""
+# tyil=int(input("Tug'ulgan yilingizni kriting?\n>>> "))
+# jyil=int(input("Joriy yilni kriting?\n>>> "))
+# yosh_hisobla(tyil, jyil)
+
+
+# def talaba_haqida(ism, familya, telefon, tugulgan_yil, joriy_yil=2022):
+#     """Foydalanuvchi haqida malumot"""
+#     print(
+#           f"Foydalanuvchi ismi: {ism.title()}\n "
+#           f"Foydalanuvchi familyasi: {familya.title()}\n "
+#           f"Siz {joriy_yil-tugulgan_yil} yoshdasiz\n "
+#           f"Foydalanuvchi telefoni {telefon}\n "
+#           )
+
+# talaba_haqida("javohir", "abdug'afurov", 971735533, tugulgan_yil=2006, joriy_yil=2022)
+
+
+
+
+# Uy ishi
+
+# def foydalanuvchi_haqida(ism, familya, tugulgan_yil, joriy_yil=2022):
+#     """Foydalanuvchi haqida malumot"""
+#     print(
+#           f"Foydalanuvchi ismi: {ism.title()}\n "
+#           f"Foydalanuvchi familyasi: {familya.title()}\n "
+#           f"Siz {joriy_yil-tugulgan_yil} da tug'ulgansiz\n "
+#           )
+
+# foydalanuvchi_haqida("javohir", "abdug'afurov", tugulgan_yil=16, joriy_yil=2022)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
