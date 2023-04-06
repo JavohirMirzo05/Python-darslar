2dc0dec22c5a520a1e2907d9e731aaed10512b3c
javohir <127494412+JavohirMirzo05@users.noreply.github.com>
2023-04-06 15:04:23 +0500

    21-dars2.py
    21-dars2.py

---
+++ 21-dars2.py
@@ -0,0 +1,37 @@
+# import avto_dars_mod 
+
+# avto1=avto_dars_mod.avto_info("GM", "Malibul", "Qora", "avtomat", 20020, 40000)
+# avto_dars_mod.avto_info(avto1)
+
+
+
+
+# import avto_dars_mod as adm
+
+# avto1=adm.avto_info("GM", "Malibu", "Qora", "avtomat", 2020, 40000)
+# adm.avto_info(avto1)
+
+
+import avto_dars_mod as adm
+
+avto1=adm.avto_info()
+
+for avto in avto1:
+    adm.info_print(avto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
